7e912cd2fa9981f7278b411bc861d007fb3df040
Ana Fdez <afminguela@gmail.com>
2024-04-24 10:57:36 +0200

    me falta el fondo, es responsive (mas o menos), no funciona el toggle, y el JS no furula, pero todo bien

--- index.js
+++ index.js
@@ -1,60 +1,82 @@
-let tareasContainer = document.querySelector(".projectContainer"); // div de 4 columnas
-let tareas = JSON.parse(localStorage.getItem("projects"));
-console.log(projects);
+let tareasContainer = document.querySelector(".tareasContainer"); // div de 4 columnas
+let tareas = JSON.parse(localStorage.getItem("tareas"));
+console.log("he llegado a la linea 3");
 
-// class Project (titulo, imageURL, descripcion) {
+class Tarea  {
 
-//   constructor (){
-//        this.titulo = document.querySelector(".form-title").value
-//        this.imageURL= document.querySelector(".form-image").value
-//        this.descripcion = document.querySelector(".form-description").value
-//   }
-// }
+  constructor  (nombre, asignada, fecha, descripcion, urgente){
+       this.nombre = document.querySelector(".tareaNombre").value
+       this.asignada= document.querySelector(".tareaAsignada").value
+       this.fecha = document.querySelector(".tareaFecha").value
+       this.descripcion = document.querySelector(".tareaDescripcion").value
+        this.urgente = document.querySelector(".tareaUrgente").value
+  }
+}
+console.log("estoy antes del printTarea");
 
-// function printProyectCards(imageURL) {
-//   return `<div class='project rounded-md' onClick=showCaseProject("${imageTitle}")><img class='rounded-md' src='${imageURL}' /></div>`;
-// }
+function printTareaCards(objetoTarea) {
+    if (objetoTarea.urgente) {
+    return `<div class="ObjetoTarea w-80 h-80 relative bg-red-500">
+   
+    <div class="tareaDescripcion w-64 h-7 left-[19.22px] top-[104.73px] absolute text-blue-800 text-2xl font-normal font-['Indie Flower']">${objetoTarea.descripcion}</div>
+    <div class="tareaAsignada w-36 h-7 left-[28px] top-[58px] absolute text-blue-800 text-lg font-normal font-['Indie Flower']">${objetoTarea.asignada}</div>
+    <div class="tareaFecha w-28 h-7 left-[176px] top-[16px] absolute text-blue-800 text-lg font-normal font-['Indie Flower']">${objetoTarea.fecha}</div>
+    <div class="tareaNombre w-64 h-8 left-[34px] top-[16px] absolute text-blue-800 text-lg font-normal font-['Indie Flower']">${objetoTarea.nombre}</div>
+  </div>`;
+  } else if (!objetoTarea.urgente) {
+  return `<div class="ObjetoTarea w-80 h-80 relative bg-zinc-300">
+ 
+<div class="tareaDescripcion w-64 h-7 left-[19.22px] top-[104.73px] absolute text-blue-800 text-2xl font-normal font-['Indie Flower']">${objetoTarea.descripcion}</div>
+<div class="tareaAsignada w-36 h-7 left-[28px] top-[58px] absolute text-blue-800 text-lg font-normal font-['Indie Flower']">${objetoTarea.asignada}</div>
+<div class="tareaFecha w-28 h-7 left-[176px] top-[16px] absolute text-blue-800 text-lg font-normal font-['Indie Flower']">${objetoTarea.fecha}</div>
+<div class="tareaNombre w-64 h-8 left-[34px] top-[16px] absolute text-blue-800 text-lg font-normal font-['Indie Flower']">${objetoTarea.nombre}</div>
+</div>`;
 
-if (projects) {
-  for (let project of projects) {
+}}
+
+if (tareas) {
+  for (let tarea of tareas) {
     //projectContainer.innerHTML += printProyectCards(project.imageURL);
-    projectContainer.innerHTML += `<div class='project rounded-md' onClick="showCaseProject('${project.imageTitle}')">
-    <img class='rounded-md' src='${project.imageURL}' />
-    </div>`;
+    tareasContainer.innerHTML += printTareaCards(objetoTarea);
   }
 }
 
-function showCaseProject(projectTitle) {
-  console.log("hola")
-  let projectoDestacado = projects.find(
-    (project) => project.imageTitle === projectTitle
-  );
-  //console.log(projectoDestacado);
-  let divDondeMetoLoDestacado = document.querySelector(".destacado");
+console.log("he pasado el print tarea")
 
-  //divDondeMetoLoDestacado.classList.add("muy-destacado"); // mirara despacito luego. te crea una clase en el document.querySelector al que apunta.
-  divDondeMetoLoDestacado.innerHTML = `<div class="muy-destacado"> <h3>${projectoDestacado.imageTitle}</h3> <img src='${projectoDestacado.imageURL}'/> <p>${projectoDestacado.imageDescription}</p> </div>`;
+// function showCaseProject(tareaNombre) {
+//   console.log("hola")
+//   let projectoDestacado = projects.find(
+//     (project) => project.imageTitle === projectTitle
+//   );
+//   //console.log(projectoDestacado);
+//   let divDondeMetoLoDestacado = document.querySelector(".destacado");
 
-}
+//   //divDondeMetoLoDestacado.classList.add("muy-destacado"); // mirara despacito luego. te crea una clase en el document.querySelector al que apunta.
+//   divDondeMetoLoDestacado.innerHTML = `<div class="muy-destacado"> <h3>${projectoDestacado.imageTitle}</h3> <img src='${projectoDestacado.imageURL}'/> <p>${projectoDestacado.imageDescription}</p> </div>`;
 
-function submitProject() {
-  let projectsList = [];
-  let projectData = {};
-  let imageURL = document.querySelector(".form-URL").value; //captura de datos
-  let imageTitle = document.querySelector(".form-title").value;
-  let imageDescription = document.querySelector(".form-description").value;
-if(!imageURL||!imageTitle){
+// }
+
+function submitTarea() {
+  let listadeTareas = [];
+  let tareaData = {};
+  let tareaNombre = document.querySelector(".tareaNombre").value //captura de datos
+  let tareaAsignada = document.querySelector(".tareaAsignada").value
+  let tareaFecha = document.querySelector(".tareaFecha").value
+  let tareaDescripcion = document.querySelector(".tareaDescripcion").value
+  let tareaUrgente = document.querySelector(".tareaUrgente").value
+if(!tareaNombre||!tareaAsignada||!tareaFecha||!tareaDescripcion||!tareaUrgente){
    console.log("hola en el IF díscolo")
-   //return mensaje;
-}
-  projectData = { imageURL, imageTitle, imageDescription }; //aqui instanciamos el objeto nuevo
+   return alert("Por favor, rellena todos los campos")
+};
 
-  if (projects) {
-    let projectsFromLocalStorage = JSON.parse(localStorage.getItem("projects"));
-    projectsList.push(...projectsFromLocalStorage, projectData);
-    return localStorage.setItem("projects", JSON.stringify(projectsList));
+  const objetoTarea = new Tarea (nombre, asignada, fecha, descripcion, urgente ); //aqui instanciamos el objeto nuevo
+  console.log(objetoTarea)
+  if (tareas) {
+    let tareasFromLocalStorage = JSON.parse(localStorage.getItem("tareas"));
+    listadeTareas.push(...tareasFromLocalStorage, tareaData);
+    return localStorage.setItem("tareas", JSON.stringify(listadeTareas));
   }
-  projectsList.push(projectData);
-  localStorage.setItem("projects", JSON.stringify(projectsList)); //
+  listadeTareasList.push(tareaData);
+  localStorage.setItem("tareas", JSON.stringify(listadeTareas)); //
   //  y subimos a localStorage
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,40 @@
 module.exports = {
   content: ["./index.html", "./input.css"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'papel': "url('src/textura papel blanco.jpg')",
+      
+      },
+      colors: {
+        mediumblue: "#040bb6",
+        "fills-tertiary": "rgba(118, 118, 128, 0.12)",
+        "colors-blue": "#007aff",
+        "colors-blue-dark": "#0a84ff",
+        white: "#fff",
+        "colors-green": "#34c759",
+      },
+      spacing: {},
+      fontFamily: {
+        "indie-flower": "'Indie Flower'",
+        "sf-pro": "'SF Pro'",
+      },
+      borderRadius: {
+        "11xs-6": "1.6px",
+        "mini-5": "14.5px",
+        "81xl": "100px",
+      },
+    },
+    fontSize: {
+      "8xs-6": "0.288rem",
+      "8xl-3": "1.706rem",
+      "mid-4": "1.088rem",
+      mid: "1.063rem",
+      inherit: "inherit",
+    },
   },
-  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 };
+    
